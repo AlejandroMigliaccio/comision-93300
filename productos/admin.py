0f4e21a1ef98aa096f3df429fcb03e3974b7afd5
Alejandro Damian Migliaccio <102888662+AlejandroMigliaccio@users.noreply.github.com>
2026-06-19 14:50:23 -0300

    update

--- productos/admin.py
+++ productos/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
-from productos.models import Productos
+from productos.models import Productos, Orden, OrdenItem
 
-#admin.site.register(Productos)
 
 @admin.register(Productos)
 class ProductosAdmin(admin.ModelAdmin):
-    list_display = ("titulo","descripcion","precio")
-
+    list_display = ("titulo", "descripcion", "precio")
     list_display_links = ("titulo",)
-
-    search_fields= ("sku", "categoria")
-
+    search_fields = ("sku", "categoria")
     list_filter = ("fecha_alta",)
-
     ordering = ("categoria",)
+
+
+class OrdenItemInline(admin.TabularInline):
+    model = OrdenItem
+    extra = 0
+    readonly_fields = ("subtotal",)
+    fields = ("producto", "cantidad", "precio_unitario", "subtotal")
+
+
+@admin.register(Orden)
+class OrdenAdmin(admin.ModelAdmin):
+    list_display = ("codigo", "fecha", "total")
+    list_display_links = ("codigo",)
+    readonly_fields = ("codigo", "fecha", "total")
+    ordering = ("-fecha",)
+    inlines = [OrdenItemInline]
